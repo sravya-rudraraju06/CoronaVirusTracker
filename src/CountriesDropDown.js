f2100bb0de5ede2d52b253225f11f19005749abf
sravya-rudraraju06 <106981368+sravya-rudraraju06@users.noreply.github.com>
2022-10-04 14:03:25 +0530

    Update CountriesDropDown.js
    changed

--- src/CountriesDropDown.js
+++ src/CountriesDropDown.js
@@ -1,26 +1,18 @@
 import React,{useState} from "react";
 
 function CountriesDropDown (props){
-
-    const [countries,setCountries] = useState([])    //data holding countries(functions settings countries )which is USESTATE_HOOK which is in react
-    // we r passing dynamically countries through usestae and passing res in down WITH SetCOUNTRIES..
-    // onchange is holding e parameter e , as it is in select box
     const onChange = (e)=>{    
-        console.log(e.target.value)        // it  will print the value which the user has been selceted. 
-        
-        // onchange after e.target.value 
-        // we need to pass props above  and selected country coming from app.js
+        console.log(e.target.value)       
         props.selectedCountry(e.target.value)
     }
     const createSelectItems = ()=>{
         const res = fetch("https://api.covid19api.com//countries")
-                    .then((res)=> res.json())            // handles with promise (.then ) convert  res into json)
-                    .then((res)=>{                       // return res to us
+                    .then((res)=> res.json())         
+                    .then((res)=>{                       
                         console.log(res)
                         setCountries(res)
                     })
         let items = []          
-         // list of countries holding
         countries.forEach(country =>{
             items.push(
                 <option key = {country.Slug} value ={country.Slug}> 
